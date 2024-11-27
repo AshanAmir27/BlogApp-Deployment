feaae59522233801cc2f64bcfb984b59478763f1
Ashan Amir <115730046+AshanAmir27@users.noreply.github.com>
2024-11-27 01:16:29 -0800

    updated code

--- Frontend/blog-app/src/components/Login.js
+++ Frontend/blog-app/src/components/Login.js
@@ -50,7 +50,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "https://blogapplication-29oz.onrender.com//api/admin/login",
+        "https://blogapplication-29oz.onrender.com/api/admin/login",
         {
           username: formData.username,
           password: formData.password,
